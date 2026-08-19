bc0d1e951e3df509c84a8fd4dfc0499ad9e1bca4
JuniorCorzo <josedanielmmf@gmail.con>
2026-08-19 17:44:24 -0500

    fix(analytics): place Analytics component inside body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,10 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
-      <Analytics></Analytics>
+      <body className="flex min-h-full flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
